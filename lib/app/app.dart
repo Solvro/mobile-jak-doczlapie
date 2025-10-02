12cbbbef4f427340e451456fbe19ff00ef150d44
Szymon Kowaliński <szymon@kowalinski.dev>
2025-10-02 10:25:33 +0200

    chore: run formatter

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:jak_doczlapie/app/router.dart';
-import 'package:jak_doczlapie/app/theme.dart';
+import "package:flutter/material.dart";
+import "router.dart";
+import "theme.dart";
 
 class App extends StatelessWidget {
   final _appRouter = AppRouter();
@@ -11,10 +11,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = AppTheme();
 
-    return MaterialApp.router(
-      theme: theme.light,
-      darkTheme: theme.dark,
-      routerConfig: _appRouter.config(),
-    );
+    return MaterialApp.router(theme: theme.light, darkTheme: theme.dark, routerConfig: _appRouter.config());
   }
 }
