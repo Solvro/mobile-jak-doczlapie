1171de9aacfff7820c14a0bf5fc806961edb1c73
tomasz-trela <trela.tomasz@protonmail.com>
2025-10-05 11:35:06 +0200

    fix(web): theme

--- lib/main.web.dart
+++ lib/main.web.dart
@@ -3,14 +3,18 @@ import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:intl/date_symbol_data_local.dart";
 import "app/app.dart";
+import "app/theme.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting("PL_pl");
+  final theme = AppTheme();
 
   runApp(
     ProviderScope(
       child: MaterialApp(
+        theme: theme.light,
+        darkTheme: theme.dark,
         home: Scaffold(
           body: Stack(
             children: [
